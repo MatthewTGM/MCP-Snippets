740a47f86242cdad513cee542f84bd2afe3399f5
MatthewTGM <62822374+MatthewTGM@users.noreply.github.com>
2020-06-15 14:35:06 +0200

    Add files via upload

---
+++ SMALL-SNIPPETS/name-above-head/Render.java
@@ -0,0 +1,7 @@
+/*
+* LOOK AT README
+*/
+
+if(entity instanceof EntityPlayerSP) {
+        	this.renderLivingLabel(entity, Minecraft.getMinecraft().getSession().getUsername(), x, y, z, 64);
+        }
---
+++ SMALL-SNIPPETS/name-above-head/readme.md
@@ -0,0 +1,3 @@
+# NAME ABOVE HEAD
+## how to add
+in the Render.java class go to the "doRender" method and underneath the base code inside the method add the content of the Render.java above :D
