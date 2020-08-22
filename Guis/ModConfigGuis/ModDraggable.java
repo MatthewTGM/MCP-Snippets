5ee6e510e5b8db0e6b75d10061121821cc78eda8
MatthewTGM <62822374+MatthewTGM@users.noreply.github.com>
2020-08-23 00:50:58 +0200

    Update ModDraggable.java

--- Guis/ModConfigGuis/ModDraggable.java
+++ Guis/ModConfigGuis/ModDraggable.java
@@ -1,6 +1,16 @@
 //ADD THIS TO LINES 33-36 (INBETWEEN THE SAVE AND GETFOLDER METHODS)
 
 	@Override
-	public boolean renderBorder(boolean bool) {
+	public int borderColor() {
+		return new Color(199, 199, 0, 80).getRGB();
+	}
+	
+	@Override
+	public int borderBackgroundColor() {
+		return new Color(199, 199, 0, 160).getRGB();
+	}
+	
+	@Override
+	public boolean renderBorder() {
 		return false;
 	}
