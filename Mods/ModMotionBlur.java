bc8062bc71517a400203f27239466efcc647f456
MatthewTGM <62822374+MatthewTGM@users.noreply.github.com>
2020-08-23 14:27:57 +0200

    Update ModMotionBlur.java

--- Mods/ModMotionBlur.java
+++ Mods/ModMotionBlur.java
@@ -1,1 +1,29 @@
-{\rtf1}
+package ga.matthewtgm.mods.impl;
+
+import ga.matthewtgm.gui.hud.ScreenPosition;
+import ga.matthewtgm.mods.ModConfig;
+import ga.matthewtgm.mods.ModDraggable;
+import net.minecraft.util.ResourceLocation;
+
+public class ModMotionBlur extends ModDraggable {
+
+	//public static boolean isEnabled = ModConfig.loadToggleFromFile("ModMotionBlur");
+	
+	@Override
+	public int getWidth() {
+		return font.getStringWidth("[Motion Blur Toggled]");
+	}
+
+	@Override
+	public int getHeight() {
+		return font.FONT_HEIGHT;
+	}
+
+	@Override
+	public void render(ScreenPosition pos) {
+		//if(isEnabled) {
+			font.drawString("[Motion Blur Toggled]", pos.getAbsoluteX(), pos.getAbsoluteY(), -1);
+			mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blur.json"));
+		//}
+	}
+}
