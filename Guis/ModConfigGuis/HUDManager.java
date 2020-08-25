f6002300722a868a1e9386844dd03720367b7c3e
MatthewTGM <62822374+MatthewTGM@users.noreply.github.com>
2020-08-25 10:11:49 +0200

    Update HUDManager.java

--- Guis/ModConfigGuis/HUDManager.java
+++ Guis/ModConfigGuis/HUDManager.java
@@ -1,6 +1,6 @@
 //ADD THIS TO LINES 79-82 (INBETWEEN `renderer.render(pos)` AND `if(pos == null)`)
 
     if(renderer.renderBorder() == true) {
-			Gui.drawRect(pos.getAbsoluteX(), pos.getAbsoluteY(), pos.getAbsoluteX() + renderer.getWidth(), pos.getAbsoluteY() + renderer.getHeight(), renderer.borderBackgroundColor());
-            Utility.drawHollowRect(pos.getAbsoluteX(), pos.getAbsoluteY(), renderer.getWidth(), renderer.getHeight(), renderer.borderColor());
+			Gui.drawRect(pos.getAbsoluteX(), pos.getAbsoluteY(), pos.getAbsoluteX() + renderer.getWidth(), pos.getAbsoluteY() + renderer.getHeight(), new Color(199, 199, 0, 80).getRGB());
+            Utility.drawHollowRect(pos.getAbsoluteX(), pos.getAbsoluteY(), renderer.getWidth(), renderer.getHeight(), new Color(199, 199, 0, 160).getRGB());
 		}
